b151dba97fd3fc3ad73f71b9ed5e1a386dc05c2e
Ignacio Inostroza <iinostroza1@miuandes.cl>
2023-10-14 18:23:16 -0300

    bug fix and rainbow

--- game.py
+++ game.py
@@ -12,6 +12,8 @@ pygame.display.set_caption("Pong Game")
 
 # Colors
 WHITE = (255, 255, 255)
+RAINBOW_COLORS = [(255, 0, 0), (255, 127, 0), (255, 255, 0), (0, 255, 0), (0, 0, 255), (75, 0, 130)]
+rainbow_index = 0
 
 # Paddles
 PADDLE_WIDTH = 15
@@ -25,18 +27,29 @@ BALL_WIDTH = 25
 ball = pygame.Rect(WIDTH // 2 - BALL_WIDTH // 2, HEIGHT // 2 - BALL_WIDTH // 2, BALL_WIDTH, BALL_WIDTH)
 ball_speed_x = 2
 ball_speed_y = 2
-speed_increment = 0.005
+speed_increment = 0.001
 
 # Score
 score1 = 0
 score2 = 0
 font = pygame.font.Font(None, 36)
 
-# Initialize ball_color
+# Initialize ball_color, paddle_color1, and paddle_color2
 ball_color = (255, 255, 255)
-
 # Create a clock object to control the frame rate
 clock = pygame.time.Clock()
+
+# Goal screen settings
+show_goal_screen = False
+goal_screen_timer = 0
+
+# Function to generate a random color
+def random_color():
+    while True:
+        color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        # Avoid very dark colors (close to black)
+        if sum(color) > 100:
+            return color
 
 # Main game loop
 while True:
@@ -45,55 +58,79 @@ while True:
             pygame.quit()
             sys.exit()
 
-    keys = pygame.key.get_pressed()
-    # Control player 1 with keyboard
-    if keys[pygame.K_w] and paddle1.top > 0:
-        paddle1.y -= 5
-    if keys[pygame.K_s] and paddle1.bottom < HEIGHT:
-        paddle1.y += 5
+    if show_goal_screen:
+        # Display "GOAL" screen for 2 seconds
+        if goal_screen_timer < 120:  # 2 seconds (60 frames per second)
+            goal_screen_timer += 1
+        else:
+            show_goal_screen = False
+            goal_screen_timer = 0
+            # Reset the ball and continue the game
+            ball = pygame.Rect(WIDTH // 2 - BALL_WIDTH // 2, HEIGHT // 2 - BALL_WIDTH // 2, BALL_WIDTH, BALL_WIDTH)
+            ball_speed_x = 2
+            ball_speed_y = 2
+            # Set random colors for ball and paddles after a goal
+            rainbow_index = 0  # Reset the rainbow index
+            ball_color = random_color()
 
-    # Control player 2 with mouse
-    mouse_pos = pygame.mouse.get_pos()
-    paddle2.y = mouse_pos[1] - PADDLE_HEIGHT // 2
+    else:
+        keys = pygame.key.get_pressed()
+        # Control player 1 with keyboard
+        if keys[pygame.K_w] and paddle1.top > 0:
+            paddle1.y -= 5
+        if keys[pygame.K_s] and paddle1.bottom < HEIGHT:
+            paddle1.y += 5
 
-    # Move the ball
-    ball.x += ball_speed_x
-    ball.y += ball_speed_y
+        # Control player 2 with mouse
+        mouse_pos = pygame.mouse.get_pos()
+        paddle2.y = mouse_pos[1] - PADDLE_HEIGHT // 2
 
-    # Collisions with paddles
-    if ball.colliderect(paddle1) or ball.colliderect(paddle2):
-        ball_speed_x *= -1
+        # Ensure that the paddle2 doesn't go beyond the window boundaries
+        if paddle2.top < 0:
+            paddle2.top = 0
+        if paddle2.bottom > HEIGHT:
+            paddle2.bottom = HEIGHT
 
-    # Collisions with the window edges
-    if ball.top <= 0 or ball.bottom >= HEIGHT:
-        ball_speed_y *= -1
+        # Move the ball
+        ball.x += ball_speed_x
+        ball.y += ball_speed_y
 
-    # Score handling
-    if ball.left <= 0:
-        score2 += 1
-        ball = pygame.Rect(WIDTH // 2 - BALL_WIDTH // 2, HEIGHT // 2 - BALL_WIDTH // 2, BALL_WIDTH, BALL_WIDTH)
-        ball_speed_x = 2
-        ball_speed_y = 2
-        ball_color = (random.randint(1, 255), random.randint(1, 255), random.randint(1, 255))
-    elif ball.right >= WIDTH:
-        score1 += 1
-        ball = pygame.Rect(WIDTH // 2 - BALL_WIDTH // 2, HEIGHT // 2 - BALL_WIDTH // 2, BALL_WIDTH, BALL_WIDTH)
-        ball_speed_x = 2
-        ball_speed_y = 2
-        ball_color = (random.randint(1, 255), random.randint(1, 255), random.randint(1, 255))
+        # Collisions with paddles
+        if ball.colliderect(paddle1) or ball.colliderect(paddle2):
+            ball_speed_x *= -1
 
-    # Gradually increase ball speed
-    ball_speed_x += speed_increment
-    ball_speed_y += speed_increment
+        # Collisions with the window edges
+        if ball.top <= 0 or ball.bottom >= HEIGHT:
+            ball_speed_y *= -1
+
+        # Score handling
+        if ball.left <= 0:
+            score2 += 1
+            show_goal_screen = True  # Show "GOAL" screen
+        elif ball.right >= WIDTH:
+            score1 += 1
+            show_goal_screen = True  # Show "GOAL" screen
+
+        # Gradually increase ball speed
+        ball_speed_x += speed_increment
+        ball_speed_y += speed_increment
 
     # Limit the frame rate to 60 frames per second using clock.tick(60)
     clock.tick(60)
 
     # Draw on the screen
     win.fill((0, 0, 0))
-    pygame.draw.rect(win, WHITE, paddle1)
-    pygame.draw.rect(win, WHITE, paddle2)
-    pygame.draw.ellipse(win, ball_color, ball)  # Set the color to the random color
+    pygame.draw.rect(win, ball_color, paddle1)
+    pygame.draw.rect(win, ball_color, paddle2)
+    pygame.draw.ellipse(win, ball_color, ball)
     score_text = font.render(f"{score1} - {score2}", True, WHITE)
     win.blit(score_text, (WIDTH // 2 - score_text.get_width() // 2, 20))
+
+    if show_goal_screen:
+        # Display "GOAL" screen in the center with rainbow colors
+        goal_text = font.render("* * * * * * GOAL * * * * * *", True, RAINBOW_COLORS[rainbow_index])
+        win.blit(goal_text, (WIDTH // 2 - goal_text.get_width() // 2, HEIGHT // 2 - goal_text.get_height() // 2))
+        # Cycle through rainbow colors
+        rainbow_index = (rainbow_index + 1) % len(RAINBOW_COLORS)
+        
     pygame.display.flip()
